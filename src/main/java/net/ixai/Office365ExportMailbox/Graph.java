7449f82c5dd0f039b1665617e30905a24700ae50
alexraffy <alex@ixai.net>
2025-04-07 23:56:51 +0200

    graph example

--- src/main/java/net/ixai/Office365ExportMailbox/Graph.java
+++ src/main/java/net/ixai/Office365ExportMailbox/Graph.java
@@ -1,4 +1,66 @@
 package net.ixai.Office365ExportMailbox;
 
+
+import java.util.List;
+import java.util.Properties;
+import java.util.function.Consumer;
+
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.identity.DeviceCodeCredential;
+import com.azure.identity.DeviceCodeCredentialBuilder;
+import com.azure.identity.DeviceCodeInfo;
+import com.microsoft.graph.models.BodyType;
+import com.microsoft.graph.models.EmailAddress;
+import com.microsoft.graph.models.ItemBody;
+import com.microsoft.graph.models.Message;
+import com.microsoft.graph.models.MessageCollectionResponse;
+import com.microsoft.graph.models.Recipient;
+import com.microsoft.graph.models.User;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
+import com.microsoft.graph.users.item.sendmail.SendMailPostRequestBody;
+
 public class Graph {
+    private static Properties _properties;
+    private static DeviceCodeCredential _deviceCodeCredential;
+    private static GraphServiceClient _userClient;
+
+    public static void initializeGraphForUserAuth(Properties properties, Consumer<DeviceCodeInfo> challenge) throws Exception {
+        // Ensure properties isn't null
+        if (properties == null) {
+            throw new Exception("Properties cannot be null");
+        }
+
+        _properties = properties;
+
+        final String clientId = properties.getProperty("app.clientId");
+        final String tenantId = properties.getProperty("app.tenantId");
+        final String[] graphUserScopes = properties.getProperty("app.graphUserScopes").split(",");
+
+        _deviceCodeCredential = new DeviceCodeCredentialBuilder()
+                .clientId(clientId)
+                .tenantId(tenantId)
+                .challengeConsumer(challenge)
+                .build();
+
+        _userClient = new GraphServiceClient(_deviceCodeCredential, graphUserScopes);
+    }
+
+
+    public static String getUserToken() throws Exception {
+        // Ensure credential isn't null
+        if (_deviceCodeCredential == null) {
+            throw new Exception("Graph has not been initialized for user auth");
+        }
+
+        final String[] graphUserScopes = _properties.getProperty("app.graphUserScopes").split(",");
+
+        final TokenRequestContext context = new TokenRequestContext();
+        context.addScopes(graphUserScopes);
+
+        final AccessToken token = _deviceCodeCredential.getTokenSync(context);
+        return token.getToken();
+    }
+
+
 }
